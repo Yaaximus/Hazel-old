a709e959061c49fff17e95a201efb907f9a0d453
Yaaximus <yasim.ahmed63@yahoo.com>
2020-10-15 18:52:10 +0500

    Looging module added

--- src/Hazel/EntryPoint.h
+++ src/Hazel/EntryPoint.h
@@ -6,6 +6,13 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv){
 
+	Hazel::Log::Init();
+	HZ_CORE_INFO("Initilized Logger!");
+	HZ_CLIENT_INFO("Initilized Logger!");
+
+	int a = 5;
+	HZ_CLIENT_INFO("Hello! Variable is :{0}", a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
