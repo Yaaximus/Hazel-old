ae85a25f364bab8712d5c1b98c750295e7129351
Yasiim Ahmad <yasim.ahmed63@yahoo.com>
2020-10-22 03:23:22 -0700

    Event System added

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include"Hazel/Events/ApplicationEvent.h"
+#include"Log.h"
+
 namespace Hazel {
 
 	Application::Application() {
@@ -11,6 +14,17 @@ namespace Hazel {
 	}
 
 	void Application::Run() {
+		
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_CORE_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HZ_CORE_TRACE(e);
+		}
+		
 		while (true);
 	}
 }
